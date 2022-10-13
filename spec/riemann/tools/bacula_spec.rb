2b26666a97220dbcdf631461a805b5ebad534c1c
Romain Tartière <romain@blogreen.org>
2022-10-12 14:03:12 -1000

    Add test for sent events

--- spec/riemann/tools/bacula_spec.rb
+++ spec/riemann/tools/bacula_spec.rb
@@ -3,8 +3,8 @@
 require 'riemann/tools/bacula'
 
 RSpec.describe Riemann::Tools::Bacula do
-  {
-    'example-1' => {
+  let(:example1_data) do
+    {
       'Build OS'              => 'x86_64-pc-linux-gnu debian bullseye/sid',
       'JobId'                 => 14182,
       'Job'                   => 'rdc10235a.2022-09-14_14.26.22_03',
@@ -46,14 +46,32 @@ RSpec.describe Riemann::Tools::Bacula do
       'Client Version'        => '9.0.6',
       'FileSet time'          => '2022-09-09 02:05:00',
       'Job Name'              => 'rdc10235a',
-    },
-  }.each do |config, expected_data|
-    context config do
+    }
+  end
+
+  describe '#parse' do
+    context 'with example-1' do
       subject { described_class.new.parse(text) }
 
-      let(:text) { File.read("spec/fixtures/#{config}") }
+      let(:text) { File.read('spec/fixtures/example-1') }
 
-      it { is_expected.to include(expected_data) }
+      it { is_expected.to include(example1_data) }
     end
+  end
+
+  describe '#send_events' do
+    subject(:instance) { described_class.new }
+
+    before do
+      allow(instance).to receive(:report)
+      instance.send_events(example1_data)
+    end
+
+    it { is_expected.to have_received(:report).with(service: 'backup rdc10235a Full (upgraded from Incremental) fd files written', metric: 1.0, tags: ['bacula']) }
+    it { is_expected.to have_received(:report).with(service: 'backup rdc10235a Full (upgraded from Incremental) sd files written', metric: 1.0, tags: ['bacula']) }
+    it { is_expected.to have_received(:report).with(service: 'backup rdc10235a Full (upgraded from Incremental) fd bytes written', metric: 6728.0, tags: ['bacula']) }
+    it { is_expected.to have_received(:report).with(service: 'backup rdc10235a Full (upgraded from Incremental) sd bytes written', metric: 6852.0, tags: ['bacula']) }
+    it { is_expected.to have_received(:report).with(service: 'backup rdc10235a Full (upgraded from Incremental) sd errors', metric: 0.0, tags: ['bacula']) }
+    it { is_expected.to have_received(:report).with(service: 'backup rdc10235a termination', state: 'ok', description: 'Backup OK', tags: ['bacula']) }
   end
 end
